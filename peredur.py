cdedc422df30aa63471e489a29fb3625251c9cf9
robert <robert@numerated.com>
2019-06-16 20:40:04 -0400

    remove last unnecessary 0 row from every matrix

--- peredur.py
+++ peredur.py
@@ -7,16 +7,15 @@ def matrix_creation(n):
 	'''
 
 	# create a matrix for the number of sheep
-	m = eye(n)
+	m = eye(n+1)
+	m.row_del(n)
 	m = m.col_insert(n, zeros(n, 1))
-	m[0, n] = n
-	if n > 2:
-		for i in range(1,n):
-			m[i, i+1] = -i
-			m[i, i-1] = -(n-i)
-			m[i, i] = n/2
+	m[0, n+1] = n
+	for i in range(1, n):
+		m[i, i+1] = -i
+		m[i, i-1] = -(n-i)
+		m[i, i] = n
 	return m 
-	
 
 
-print(matrix_creation(2))
+print(matrix_creation(1))
