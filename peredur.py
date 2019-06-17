934b01713806a033941910c193457fa02b75cf0a
robert <robert@numerated.com>
2019-06-17 09:01:26 -0400

    i dunno what these are but i need to add the outline

--- peredur.py
+++ peredur.py
@@ -1,4 +1,5 @@
 from sympy import *
+import numpy as np
 
 
 def matrix_creation(n):
@@ -8,14 +9,22 @@ def matrix_creation(n):
 
 	# create a matrix for the number of sheep
 	m = eye(n+1)
-	m.row_del(n)
-	m = m.col_insert(n, zeros(n, 1))
+	m = m.col_insert(n+1, zeros(n+1, 1))
 	m[0, n+1] = n
-	for i in range(1, n):
+	m[n, n] = 1
+	for i in range(1, n-1):
 		m[i, i+1] = -i
 		m[i, i-1] = -(n-i)
 		m[i, i] = n
+	print(m)
 	return m 
 
 
-print(matrix_creation(1))
+def peredur(n):
+	matrix = matrix_creation(n)
+	rref_matrix = matrix.rref()[0]
+	print(rref_matrix)
+	max_array = np.zeros(n)
+
+
+print(peredur(2))
